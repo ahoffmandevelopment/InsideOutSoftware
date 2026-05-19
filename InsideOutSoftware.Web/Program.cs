350753fb77fac2b02e04ee5ca79e57efa0aa4be4
Andrew Hoffman <andrew.hoffman@omnicell.com>
2026-05-19 17:39:48 -0500

    Add www redirect

--- InsideOutSoftware.Web/Program.cs
+++ InsideOutSoftware.Web/Program.cs
@@ -19,6 +19,23 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    var host = context.Request.Host.Host;
+
+    if (host.StartsWith("www.", StringComparison.OrdinalIgnoreCase))
+    {
+        var newUrl =
+            $"{context.Request.Scheme}://{host.Substring(4)}{context.Request.Path}{context.Request.QueryString}";
+
+        context.Response.Redirect(newUrl, permanent: true);
+
+        return;
+    }
+
+    await next();
+});
+
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
